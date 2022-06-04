306ff04fec171f0fbee36ffacafdfe2d00d04fe1
“Daniela <dgarzonc@devco.com.co>
2022-06-04 10:30:37 -0500

    clase paciente

--- src/main/java/com/ingjuanfg/Paciente.java
+++ src/main/java/com/ingjuanfg/Paciente.java
@@ -1,4 +1,12 @@
 package com.ingjuanfg;
 
-public class Paciente {
+public class Paciente extends Persona{
+    private int habitacion;
+    private String enfermedad;
+
+    public Paciente(String nombre, String apellido, int peso, int estatura, int edad, int habitacion, String enfermedad) {
+        super(nombre, apellido, peso, estatura, edad);
+        this.habitacion = habitacion;
+        this.enfermedad = enfermedad;
+    }
 }
