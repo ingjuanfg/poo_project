747fb4754e3156da321191e23f094829ae234a45
erikar98 <erikarestrepo105@gmail.com>
2022-06-04 10:09:52 -0500

    comit herencia

---
+++ src/main/java/com/ingjuanfg/Persona.java
@@ -0,0 +1,59 @@
+package com.ingjuanfg;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int peso;
+    private int estatura;
+    private int edad;
+
+    public Persona(){
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(int estatura) {
+        this.estatura = estatura;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    public int calculoIma(int peso, int estatura){
+        return peso + estatura;
+    }
+}
