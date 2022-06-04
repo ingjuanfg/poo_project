51bce70ace9bcb2d0b68e17db61302f022206ee9
Victor <victormbp2@gmail.com>
2022-06-04 10:35:01 -0500

    Constructor Paciente

--- src/main/java/com/ingjuanfg/Paciente.java
+++ src/main/java/com/ingjuanfg/Paciente.java
@@ -1,4 +1,41 @@
 package com.ingjuanfg;
 
-public class Paciente {
+import java.util.Date;
+
+public class Paciente extends Persona{
+
+    private String diagnostico;
+    private Date fechaIngreso;
+    private boolean activo;
+
+    public Paciente(String nombre, String apellido, int edad, String diagnostico, Date fechaIngreso, boolean activo) {
+        super(nombre, apellido, edad);
+        this.diagnostico = diagnostico;
+        this.fechaIngreso = fechaIngreso;
+        this.activo = activo;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
