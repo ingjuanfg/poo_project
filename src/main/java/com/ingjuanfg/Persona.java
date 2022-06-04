4ba406a5b56b17a6c999c3d75fb02592a329ad18
Monica Ospina <mospinac@devco.com.co>
2022-06-04 10:09:57 -0500

    Añadiendo archivos

---
+++ src/main/java/com/ingjuanfg/Persona.java
@@ -0,0 +1,25 @@
+package com.ingjuanfg;
+
+public class Persona {
+    private int peso;
+    private int estatura;
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(int estatura) {
+        this.estatura = estatura;
+    }
+    public int calculoImc(int peso, int estatura){
+
+    }
+}
---
+++ src/test/java/com/ingjuanfg/PersonaTest.java
@@ -0,0 +1,13 @@
+package com.ingjuanfg;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonaTest {
+    @Test
+    public void testCalcularImc(){
+        Persona atleta = new Persona();
+        assertEquals
+
+    }
+
+}
